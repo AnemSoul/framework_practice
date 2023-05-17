85e3f5c1d72c1d178a49cd8b9f971276b2210566
Alekseii Guskov <ingrzhd@gmail.com>
2023-05-17 19:49:14 +0300

    Ready for the continues integration

--- src/test/resources/name.java
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE suite SYSTEM "http://testng.org/testng-1.0.dtd" >
-
-<suite name="Sending" verbose="1" >
-    <test name="senfing test"   >
-        <classes>
-            <class name="com.epam.training.student_Aleksei_Guskov.test.GooglePlatformPriceEqualsPriceInEmailTests" />
-        </classes>
-    </test>
-</suite>
